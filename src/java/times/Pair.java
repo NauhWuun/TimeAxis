24d56b7e57c36080696641dab0ac772638ca1d01
ZC <geteasycn@126.com>
2020-02-03 00:05:53 +0800

    new codes

---
+++ src/java/times/Pair.java
@@ -0,0 +1,20 @@
+package java.times;
+
+public class Pair<Left, Right>
+{
+    private Left left;
+    private Right right;
+
+    public Pair(Left left, Right right) {
+    	this.left = left;
+    	this.right = right;
+    }
+
+    public Left getLeft() {
+        return left;
+    }
+
+    public Right getRight() {
+        return right;
+    }
+}
