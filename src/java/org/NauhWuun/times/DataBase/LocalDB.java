60eacd4278f1dc7a8107dd23fd0bc0ce0a701fbb
NauhWuun <NauhWuun2009@gmail.com>
2020-03-21 15:58:58 +0000

    remove RockDB Model

--- src/java/org/NauhWuun/times/DataBase/LocalDB.java
+++ src/java/org/NauhWuun/times/DataBase/LocalDB.java
@@ -1,68 +1,6 @@
 package org.NauhWuun.times.DataBase;
 
-import org.rocksdb.RocksDB;
-
 public final class LocalDB
 {
-    static {
-        RocksDB.loadLibrary();
-    }
-
-    private RocksDB database;
-    private DBOptions options;
-
-    public LocalDB(String db) throws RocksDBException {
-        File file = new File(db);
-        if (! file.exists()) {
-            file.mkdirs();
-        }
-
-        options = new DBOptions()
-                      .setCreateMissingColumnFamilies(true)
-                      .setCreateIfMissing(true)
-                      .setParanoidChecks(true)
-                      .setMaxOpenFiles(8192);
-
-        database = RocksDB.open(options, db, cfd, cfh);
-    }
-
-    public RocksDB getDatabase() {
-        return database;
-    }
-
-    public RocksIterator getIter() {
-        return getIter(0);
-    }
-
-    public RocksIterator getFirst(byte[] _0) {
-        RocksIterator iterator = getIter();
-        if (_0 != null) {
-            iterator.seek(_0);
-        } else {
-            iterator.seekToFirst();
-        }
-        return iterator;
-    }
-
-    public RocksIterator getLast() {
-        RocksIterator iterator = getIter();
-        iterator.seekToLast();
-        return iterator;
-    }
-
-    public byte[] get(byte[] key) throws RocksDBException {
-        return database.get(key);
-    }
-
-    public void put(byte[] key, byte[] value) throws RocksDBException {
-        database.put(key, value);
-    }
-
-    public void delete(byte[] key) throws RocksDBException {
-        delete((byte) 0, key);
-    }
-
-    public void close() {
-        database.close();
-    }
+   
 }
